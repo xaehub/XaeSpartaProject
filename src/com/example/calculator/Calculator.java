a6df5b769459256e939388452076116030dc8ef2
xaehub <novxae@gmail.com>
2025-02-25 17:28:21 +0900

    Calculator LV1

---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,59 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("김태정이 만든 계산기입니다");
+        String goStop;
+
+
+        while (true) {
+            System.out.print("첫번째 수를 입력해 주세요: ");
+            int a = sc.nextInt();
+            System.out.print("두번째 수를 입력해 주세요: ");
+            int b = sc.nextInt();
+
+            System.out.print(" +  -  *  / 중 하나를 입력해 주세요: ");
+            char c = sc.next().charAt(0);
+
+
+            if (c == '+') {
+                int addResult = a + b;
+                System.out.println("두 수를 더한 값은: " + addResult);
+            } else if (c == '-') {
+                int minusResult = a - b;
+                System.out.println("두 수를 뺀 값은: " + minusResult);
+            } else if (c == '*') {
+                int multiplyResult = a * b;
+                System.out.println("두 수를 곱한 값은: " + multiplyResult);
+            } else if (c == '/') {
+                if (b == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
+                } else {
+                    int divideResult = a / b;
+                    System.out.println("두 수를 나눈 값은: " + divideResult);
+                }
+            } else {
+                System.out.println("올바른 기호가 아닙니다");
+            }
+
+            while (true) {
+                System.out.println("더 계산하시겠습니까?");
+                System.out.println("go 또는 exit를 선택해 주세요: ");
+                goStop = sc.next();
+
+                if (goStop.equals("exit")) {
+                    System.out.println("계산기가 종료됩니다.");
+                    return;
+                } else if (goStop.equals("go")) {
+                    break;
+                } else {
+                    System.out.println("잘못된 입력입니다. 다시 입력해 주세요");
+                }
+            }
+        }
+    }
+}
