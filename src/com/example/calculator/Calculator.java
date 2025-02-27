f34238fcd12f67b6edec7f3dc247b423f7962e4c
xaehub <novxae@gmail.com>
2025-02-27 15:57:30 +0900

    refactor: move calculation logic to Calculator.java

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -2,8 +2,39 @@ package com.example.calculator;
 
 public class Calculator {
 
-    int a;
-    int b;
-    char c;
+    // 속성
 
+
+
+    // 생성자
+    public Calculator() {
+
+    }
+
+    // 기능
+
+    public void calculator(int a, int b, char c) {
+        int result;
+
+        if (c == '+') {
+            result = a + b;
+            System.out.println("두 수를 더한 값은: " + result);
+        } else if (c == '-') {
+            result = a - b;
+            System.out.println("두 수를 뺀 값은: " + result);
+        } else if (c == '*') {
+            result = a * b;
+            System.out.println("두 수를 곱한 값은: " + result);
+        } else if (c == '/') {
+            if (b == 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
+            } else {
+                result = a / b;
+                System.out.println("두 수를 나눈 값은: " + result);
+            }
+        } else {
+            System.out.println("올바른 기호가 아닙니다");
+        }
+
+    }
 }
--- src/com/example/calculator/Main.java
+++ src/com/example/calculator/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Calculator cal = new Calculator();
 
         System.out.println("스파르타 개인 프로젝트");
         System.out.println("------ 계산기 ------");
@@ -20,28 +21,8 @@ public class Main {
             System.out.print(" +  -  *  / 중 하나를 입력해 주세요: ");
             char c = sc.next().charAt(0);
 
+            cal.calculator(a, b, c);
 
-            int result = 0;
-
-            if (c == '+') {
-                result = a + b;
-                System.out.println("두 수를 더한 값은: " + result);
-            } else if (c == '-') {
-                result = a - b;
-                System.out.println("두 수를 뺀 값은: " + result);
-            } else if (c == '*') {
-                result = a * b;
-                System.out.println("두 수를 곱한 값은: " + result);
-            } else if (c == '/') {
-                if (b == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다");
-                } else {
-                    result = a / b;
-                    System.out.println("두 수를 나눈 값은: " + result);
-                }
-            } else {
-                System.out.println("올바른 기호가 아닙니다");
-            }
 
             while (true) {
                 System.out.println("더 계산하시겠습니까?");
