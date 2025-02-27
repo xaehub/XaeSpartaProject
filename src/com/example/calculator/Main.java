fd8255343fdae180d65c3d3ab99ba6716801fc33
xaehub <novxae@gmail.com>
2025-02-27 11:06:24 +0900

    chore: update project title

--- src/com/example/calculator/Main.java
+++ src/com/example/calculator/Main.java
@@ -2,7 +2,7 @@ package com.example.calculator;
 
 import java.util.Scanner;
 
-public class Calculator {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
